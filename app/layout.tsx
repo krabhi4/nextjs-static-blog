4b7b8e9bf87dc0c1cf336ac03a777697626ae184
Kumar Abhishek <59351747+krabhi4@users.noreply.github.com>
2023-06-02 11:20:06 +0530

    removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,14 +25,6 @@ export default function RootLayout({
     </header>
   );
 
-  const footer = (
-    <footer>
-      <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
-        <h3>Designed by Kumar</h3>
-      </div>
-    </footer>
-  );
-
   return (
     <html>
       <head />
@@ -40,7 +32,7 @@ export default function RootLayout({
         <div className="mx-auto  max-w-2xl px-6">
           {header}
           {children}
-          {footer}
+          
         </div>
       </body>
     </html>
